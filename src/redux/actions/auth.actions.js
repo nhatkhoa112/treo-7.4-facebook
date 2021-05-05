87a419d2760290198e2b70e6b5d6dfeb46944b2d
Loi Van Tran <datloiboi@gmail.com>
2021-05-05 15:47:59 +0700

    Add redux thunk, api service, basic redux setup

---
+++ src/redux/actions/auth.actions.js
@@ -0,0 +1,16 @@
+import { toast } from "react-toastify";
+
+import * as types from "../constants/auth.constants";
+
+import api from "../../apiService";
+
+const signInWithEmail = (email, password) => async (dispatch) => {
+
+}
+
+
+const authActions = {
+  signInWithEmail,
+};
+
+export default authActions;
