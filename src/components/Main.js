a9311696b1117ddef1cf272fe61d0b50b28ac7f9
nhat khoa <nhatkhoa.tin@gmail.com>
2021-05-07 14:40:43 +0700

    add posts to home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import styled from 'styled-components'; 
 import {useSelector, useDispatch} from 'react-redux';
 import postsActions from '../redux/actions/posts.actions';
+import moment from 'moment';
 
 export const Main = () => {
     const postsState = useSelector(state => state.posts)
@@ -35,10 +36,35 @@ export const Main = () => {
                 </MainStoryIcons>
             </MainStory>
             <MainPosts>
-                {posts.map(post => {
+                {posts.reverse().map(post => {
                     return(
                         <MainPost key={post.id}>
-                            
+                            <MainPostUser>
+                                <MainPostUserContent>
+                                    <div>
+                                        {moment(post.createdAt).fromNow()}
+                                    </div>
+                                </MainPostUserContent>
+                                <MainPostUserIcon>
+                                    <i class="fas fa-ellipsis-h"></i>
+                                </MainPostUserIcon>
+                            </MainPostUser>
+                            <MainPostContent >
+                                <MainPostText>
+                                    <div>
+                                        {post.body}
+                                    </div>
+                                </MainPostText>
+                                {post.photos ? (
+                                    <MainPostImage>
+                                        {console.log(post.photos)}
+                                        {post.photos.map((photo,index) => {
+                                            return <img key={index} src={photo.url} alt="" />
+                                        })}
+                                    </MainPostImage>
+                                ): ("")}
+                                
+                            </MainPostContent>
                         </MainPost>
                     )
                 })}
@@ -76,6 +102,7 @@ const MainStoryInput = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 10px;
     border-bottom: 1px rgba(255, 255, 255, 0.3) solid;
     >img{
         border-radius: 50%;
@@ -140,5 +167,78 @@ const MainPosts = styled.div`
 `
 
 const MainPost= styled.div`
-    
+    width: 100%;
+    margin-top: 5px;
+    height: auto;
+    background: #242526;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
 `
+
+const MainPostUser = styled.div`
+    width: 100%;
+    height: 50px;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+`
+
+const MainPostUserContent = styled.div`
+    >div{
+        color: #ccc;
+        font-size: 13px;
+    }
+`
+
+const MainPostUserIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    background: #3A3B3C;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    :hover{
+        background: #787878;
+    }
+
+    >i{
+        color: #ccc;
+    }
+
+
+`
+
+
+const MainPostContent = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content:flex-start;
+    height: 300px;
+    overflow: hidden;
+    overflow-y: auto;
+    padding: 10px 20px;
+    
+
+`
+
+const MainPostText = styled.div`
+    width: 100%;
+    >div{
+        text-align: left;
+        color: #ccc;
+    }
+`;
+
+const MainPostImage = styled.div`
+    
+    >img{
+    width: 100%;
+    height: auto;
+    }
+`;
